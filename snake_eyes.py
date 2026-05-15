0f1ea14785d33043b583ce5b61caefa8e9a0b123
Sara <sara.taghaddosi.st@gmail.com>
2026-05-15 17:23:38 +0200

    Rename snake-eyes.py to snake_eyes.py

---
+++ snake_eyes.py
@@ -0,0 +1,15 @@
+import random
+
+total = 0
+
+while total != 2:
+  die1 = random.randint(1, 6)
+  die2 = random.randint(1, 6)
+
+  total = die1 + die2
+
+  if total == 2:
+    print("Snake eyes!")
+    break
+  else:
+    print("Nope")
